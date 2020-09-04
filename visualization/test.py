02dde6f655a7ca94b8336b0cc56b36d12512a040
Joe Russack <jrussack@calacademy.org>
2020-09-04 16:18:08 -0700

    Cleanup

--- visualization/test.py
+++ visualization/test.py
@@ -39,10 +39,6 @@ if cases_1 is None:
     cases_2 = pickle.load(open("cases_2", "rb"))
 
 
-# print(f"result 1: {returned_result[0]}")
-# print(f"result 2: {returned_result[1]}")
-# print("done!")
-
 with open("LA_CASES6_output_python.dat","w") as out:
     out.write("R,Day Farm_1,Farm_2,Farm_3,Farm_4,Mining_1,Mining_2,Mining_3,Mining_4,Utilities_1,Utilities_2,Utilities_3,Utilities_4,Construction_1,Construction_2,Construction_3,Construction_4,Manf_1,Manf_2,Manf_3,Manf_4,Wholesale_1,Wholesale_2,Wholesale_3,Wholesale_4,Retail_1,Retail_2,Retail_3,Retail_4,Transp_1,Transp_2,Transp_3,Transp_4,Information_1,Information_2,Information_3,Information_4,Financial_1,Financial_2,Financial_3,Financial_4,Prof_1,Prof_2,Prof_3,Prof_4,Ed_Hlth_1,Ed_Hlth_2,Ed_Hlth_3,Ed_Hlth_4,Leisure_1,Leisure_2,Leisure_3,Leisure_4,Other_1,Other_2,Other_3,Other_4,Gov_1,Gov_2,Gov_3,Gov_4,Farm,Mining,Utilities,Construction,Manf,Wholesale,Retail,Transp,Information,Financial,Prof,Ed_Hlth,Leisure,Other,Gov,Susceptible,Infected,Removed,total_E,Disease_only\n")
     for cur_line in cases_1:
@@ -61,6 +57,4 @@ with open("LA_CASES6_surfaces_python.dat","w") as out:
                 out.write("\n")
             else:
                 out.write(" ")
-#loader_results = j.loader()
-# print(f"{loader_results}")
-#print(f"file variable foo: {j.foo}")
+
